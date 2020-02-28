de381f2a8c0f3e51204bb6fe011db757006eccc5
LiuDaDa <15302023722@163.com>
2020-02-28 14:34:24 +0800

    初始化

---
+++ server/app.js
@@ -0,0 +1,41 @@
+const Koa = require('koa');
+const path = require('path');
+const static = require('koa-static');
+
+const app = new Koa();
+const server = require('http').Server(app.callback());
+const io = require('socket.io')(server);
+
+var clients = [];
+
+
+app.use(static(
+    path.join(__dirname, './public')
+));
+
+server.listen(4000, () => {
+    console.log('连接成功');
+});
+
+let count = 0;
+
+let countUser = {};
+
+io.on('connection', socket => {
+    
+    socket.on('login', function (msg) {
+        countUser[msg.username] = msg.id;
+        count++;
+        io.emit('login', { count, username: msg.username  });
+    });
+
+    socket.on('logout', function (msg) {
+        delete countUser[msg.username];
+        count--;
+        socket.broadcast.emit('logout', { username: msg.username, count  });
+    });
+
+    socket.on('message', function (msg) {
+        socket.broadcast.emit('message', { username: msg.nickName, message: msg.message  });
+    });
+})
--- src/App.js
+++ src/App.js
@@ -1,24 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const socket = require('socket.io-client')('http://127.0.0.1:4000');
+
 function App() {
+
+  const [isLogin, setIsLogin] = useState(false);
+  const [nickName, setNickName] = useState('');
+  const [message, setMessage] = useState('');
+  const [count, setCount] = useState(0);
+
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    init();
+  }, [list]);
+
+  function handleChange(e) {
+    setNickName(e.target.value);
+  }
+
+  function handleSubmit() {
+    if (nickName !== '') {
+      setIsLogin(true);
+      socket.emit('login', {
+        id: Date.now(),
+        username: nickName
+      });
+    }
+  }
+
+  function handleLogout() {
+    setIsLogin(false);
+    socket.emit('logout', {
+      username: nickName
+    });
+  }
+
+  function handleMessageChange(e) {
+    setMessage(e.target.value);
+  }
+
+  function init(){
+    socket.on('login', function (data) {
+      console.log(data.username + '登录了聊天室');
+      setCount(data.count)
+    });
+  
+    socket.on('logout', function (data) {
+      console.log(data.username + '离开了聊天室');
+      setCount(data.count)
+    });
+    socket.on('message', function (data) {
+      setList(list.concat([{ nickName: data.username, message: data.message, isMe: false }]));
+    });
+  }
+  
+
+  function handleSend() {
+    setList(list.concat([{ nickName, message, isMe: true }]));
+    setMessage('');
+    socket.emit('message', { nickName, message, isMe: true });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {
+        isLogin ? (
+          <>
+            <header>
+              Hdove Vip 黄金聊天室({`${count}`})
+              <span style={{ float: 'right', cursor: 'pointer' }} onClick={handleLogout}>退出</span>
+            </header>
+
+            <section>
+              <ul>
+                {
+                  list && list.length > 0 && list.map(item => {
+                    return <li className={item.isMe ? 'right' : 'left'} key={item.nickName}>
+                        <div className="logo">{[...item.nickName][0]}</div>
+                        <span className="content">{item.message}</span>
+                      </li>
+                    
+                  })
+                }
+              </ul>
+            </section>
+
+            <footer>
+              <input type="text" onChange={handleMessageChange} value={message}/>
+              <button onClick={handleSend}>发送</button>
+            </footer></>
+        ) : (
+            <div className="login">
+              <form action="" onSubmit={handleSubmit}>
+                <span>请输入昵称：
+              <input type="text" onChange={handleChange} value={nickName} />
+                </span>
+              </form>
+
+            </div>
+          )
+      }
+
     </div>
   );
 }
